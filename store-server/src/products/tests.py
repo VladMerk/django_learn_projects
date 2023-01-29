54b105652fffdc805ce57ab1c0ba3f386ead3340
Merk Vladimir <merkvladimir@gmail.com>
2023-01-29 13:54:31 +0300

    change app 'products' and test

--- store-server/src/products/tests.py
+++ store-server/src/products/tests.py
@@ -1,3 +1,37 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from products.models import ProductCategory, Product
+
+
+class ProductsTestCase(TestCase):
+
+    def setUp(self):
+        cities = [
+            ProductCategory(name='Cloth'),
+            ProductCategory(name='Shoes'),
+        ]
+        ProductCategory.objects.bulk_create(cities)
+
+    def test_str_productcategory(self):
+        city = ProductCategory.objects.get(name='Cloth')
+        self.assertEqual(str(city), 'Cloth')
+
+
+    def test_unique_productcategory(self):
+        obj = ProductCategory(name='Cloth')
+
+        with self.assertRaisesMessage(ValidationError, 'Категория продуктов должна быть уникальной'):
+            obj.full_clean()
+
+    def test_view_index(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+    def test_view_products(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('title', response.context)
+        self.assertTrue('products', response.context)
+        self.assertTrue('categories', response.context)
+        self.assertTemplateUsed(response, 'products/products.html')
--- store-server/src/products/views.py
+++ store-server/src/products/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
 
 def index(request):
     return render(request, 'products/index.html')
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'products/products.html', context=context)
